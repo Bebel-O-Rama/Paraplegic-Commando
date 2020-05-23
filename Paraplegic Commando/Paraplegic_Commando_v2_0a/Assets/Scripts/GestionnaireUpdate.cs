bb7e2f173a6f4e426db7b27ee66f83be5e952a79
flyn1nja <alexis.g.lebel@gmail.com>
2020-05-23 08:11:54 -0700

    Version 2.0a
    Now using unity XR

---
+++ Paraplegic Commando/Paraplegic_Commando_v2_0a/Assets/Scripts/GestionnaireUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    List<IUpdatable> updatablesScripts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponents<IUpdatable>(updatablesScripts);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        updatablesScripts.ForEach(x => x.UpdateObj());
+    }
+}
---
+++ Paraplegic Commando/Paraplegic_Commando_v2_0a/Assets/Scripts/WheelZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelZone : MonoBehaviour
+{
+    public bool isInZone;
+    [SerializeField]
+    bool IsOnRightSide;
+    GameObject Hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hand = IsOnRightSide ? GameObject.Find("RH") : GameObject.Find("LH");
+    }
+
+    private void OnTriggerEnter(Collider other) => IsOnRightSide = other.gameObject == Hand.gameObject ? true : IsOnRightSide;
+    private void OnTriggerExit(Collider other) => IsOnRightSide = other.gameObject == Hand.gameObject ? false : IsOnRightSide;
+}
